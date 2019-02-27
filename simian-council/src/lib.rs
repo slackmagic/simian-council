8a55cc43e6fc520ab736657f7a1a1f6fb4794f3f
SlackMagiC <laurent.pietrzyk@gmail.com>
2019-02-27 21:55:15 +0100

    Update

--- simian-council/src/lib.rs
+++ simian-council/src/lib.rs
@@ -1,41 +1,21 @@
+use crate::secret_generator::*;
 use rand::prelude::*;
-use std::error::Error;
-use std::fs::File;
-use std::io::{BufRead, BufReader};
-use std::path::Path;
+
+pub mod secret_generator;
 
 pub fn ask_for_a_secret(num: u8, bits: u8) -> String {
     let dictionary = get_dictionary();
-    let (filtered_dictionary, _) = dictionary.split_at((2 as usize).pow(bits as u32));
-
     let mut rng = rand::thread_rng();
-    let mut secret: String = "".to_string();
+    let mut secret: String = String::new();
+
+    let index = (2 as usize).pow(bits as u32);
+    let (filtered_dictionary, _) = dictionary.split_at(index);
 
     for _ in 0..num {
         let random_index = rng.gen_range(1, filtered_dictionary.len());
-        let entry: Vec<&str> = filtered_dictionary[random_index].split(' ').collect();
-        secret += entry[1];
+        secret += &filtered_dictionary[random_index].word;
         secret += " ";
     }
 
     secret
-}
-
-fn get_dictionary() -> Vec<String> {
-    let mut dictionary: Vec<String> = Vec::new();
-    let path = Path::new("./resources/wordlist");
-
-    let file = match File::open(&path) {
-        Err(why) => panic!("couldn't open {}: {}", path.display(), why.description()),
-        Ok(file) => file,
-    };
-
-    for line in BufReader::new(file).lines() {
-        match line {
-            Err(why) => panic!("couldn't read line: {}", why.description()),
-            Ok(line) => dictionary.push(line),
-        }
-    }
-
-    dictionary
 }
---
+++ simian-council/src/secret_generator.rs
@@ -0,0 +1,33 @@
+pub mod dictionary_entry;
+
+use crate::secret_generator::dictionary_entry::*;
+use std::error::Error;
+use std::fs::File;
+use std::io::{BufRead, BufReader};
+use std::path::Path;
+
+pub fn get_dictionary() -> Box<Vec<DictionaryEntry>> {
+    let mut dictionary: Vec<DictionaryEntry> = Vec::new();
+    let path = Path::new("./resources/wordlist");
+
+    let file = match File::open(&path) {
+        Err(why) => panic!("couldn't open {}: {}", path.display(), why.description()),
+        Ok(file) => file,
+    };
+
+    for line in BufReader::new(file).lines() {
+        match line {
+            Err(why) => panic!("couldn't read line: {}", why.description()),
+            Ok(line) => {
+                let entry: Vec<&str> = line.split(' ').collect();
+
+                dictionary.push(DictionaryEntry::new(
+                    entry[0].to_string(),
+                    entry[1].to_string(),
+                ));
+            }
+        }
+    }
+
+    Box::new(dictionary)
+}
