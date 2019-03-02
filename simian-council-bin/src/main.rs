860fefa955d489af6abc30f5fbbc737723e83fac
SlackMagiC <laurent.pietrzyk@gmail.com>
2019-03-02 17:53:43 +0100

    Update

--- simian-council-bin/src/main.rs
+++ simian-council-bin/src/main.rs
@@ -1,6 +1,6 @@
 extern crate simian_council;
 
 fn main() {
-    let secret = simian_council::ask_for_a_secret(10, 150, 100);
+    let secret = simian_council::ask_for_a_secret(10, 1, 100);
     println!("This is your secret : {}", secret);
 }
