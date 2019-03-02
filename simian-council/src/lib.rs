290371f91c484c4c89ffc4395e59aa660f569cde
SlackMagiC <slackmagic@excepti0n-mkii.home>
2019-03-02 09:39:06 +0100

    Cleaning

--- simian-council/src/lib.rs
+++ simian-council/src/lib.rs
@@ -1,21 +1,5 @@
-use crate::secret_generator::*;
-use rand::prelude::*;
-
 pub mod secret_generator;
 
 pub fn ask_for_a_secret(num: u8, bits: u8) -> String {
-    let dictionary = get_50k_dictionary();
-    let mut rng = rand::thread_rng();
-    let mut secret: String = String::new();
-
-    let index = (2 as usize).pow(bits as u32);
-    let (filtered_dictionary, _) = dictionary.split_at(index);
-
-    for _ in 0..num {
-        let random_index = rng.gen_range(1, filtered_dictionary.len());
-        secret += &filtered_dictionary[random_index].word;
-        secret += " ";
-    }
-
-    secret
+    secret_generator::generate(num, bits)
 }
--- simian-council/src/secret_generator.rs
+++ simian-council/src/secret_generator.rs
@@ -1,12 +1,29 @@
 pub mod dictionary_entry;
-
 use crate::secret_generator::dictionary_entry::*;
+use rand::prelude::*;
 use std::error::Error;
 use std::fs::File;
 use std::io::{BufRead, BufReader};
 use std::path::Path;
 
-pub fn get_dictionary() -> Box<Vec<DictionaryEntry>> {
+pub fn generate(num: u8, bits: u8) -> String {
+    let dictionary = get_50k_dictionary();
+    let mut rng = rand::thread_rng();
+    let mut secret: String = String::new();
+
+    let index = (2 as usize).pow(bits as u32);
+    let (filtered_dictionary, _) = dictionary.split_at(index);
+
+    for _ in 0..num {
+        let random_index = rng.gen_range(1, filtered_dictionary.len());
+        secret += &filtered_dictionary[random_index].word;
+        secret += " ";
+    }
+
+    secret
+}
+
+fn get_dictionary() -> Box<Vec<DictionaryEntry>> {
     let mut dictionary: Vec<DictionaryEntry> = Vec::new();
     let path = Path::new("./resources/wordlist");
 
@@ -32,7 +49,7 @@ pub fn get_dictionary() -> Box<Vec<DictionaryEntry>> {
     Box::new(dictionary)
 }
 
-pub fn get_50k_dictionary() -> Box<Vec<DictionaryEntry>> {
+fn get_50k_dictionary() -> Box<Vec<DictionaryEntry>> {
     let mut dictionary: Vec<DictionaryEntry> = Vec::new();
     let path = Path::new("./resources/fr_50k.txt");
 
