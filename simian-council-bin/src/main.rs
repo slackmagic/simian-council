bfdcb23bc1c2000c5b33307ae9a9df34b7fd1393
slackmagic <slack.magic@gmail.com>
2020-10-16 17:36:39 +0200

    Add WASM lib

--- simian-council-bin/src/main.rs
+++ simian-council-bin/src/main.rs
@@ -1,9 +1,9 @@
 extern crate simian_council;
 
 fn main() {
-    //println!("============================================================");
-    //let secret = simian_council::ask_for_a_secret(10, 1, 100);
-    //println!("This is your secret : {}", secret);
+    println!("============================================================");
+    let secret = simian_council::ask_for_a_secret(4, 1, 100);
+    println!("This is your secret : {}", secret);
 
     println!("============================================================");
     let pokemon = simian_council::ask_for_a_pokemon();
